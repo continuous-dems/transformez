b0b23cbcb217e766d6dc20ce77bb9c69ce05d5b4
Matthew Love <matthew.love@colorado.edu>
2026-02-20 20:33:19 -0800

    update __version__

--- src/transformez/__init__.py
+++ src/transformez/__init__.py
@@ -12,6 +12,23 @@ transformez
 __author__ = "Matthew Love"
 __credits__ = "CIRES"
 
+try:
+    from fetchez._version import __version__
+except ImportError:
+    # Fallback when using the package from source without installing
+    # in editable mode with pip (nobody should do this):
+    # <https://pip.pypa.io/en/stable/topics/local-project-installs/#editable-installs>
+    import warnings
+
+    warnings.warn(
+        "Importing 'fetchez' outside a proper installation."
+        " It's highly recommended to install the package from a stable release or"
+        " in editable mode.",
+        stacklevel=2,
+    )
+    __version__ = "dev"
+
+
 import os
 import glob
 # from .hooks import TransformezHook
